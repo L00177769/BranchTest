cbe5486347c6d061d63f41c179b3f4942202aa8f
martina.atkinson <L00177769@atu.ie>
2022-12-01 11:22:47 +0000

    first commit

---
+++ setup.cmd
@@ -0,0 +1,8 @@
+echo "# BranchTest" >> README.md 
+git init
+add README.md 
+git commit -m "first commit" 
+git branch -M main 
+git remote add origin https://github.com/L00177769/BranchTest.git
+git push -u origin main
+
